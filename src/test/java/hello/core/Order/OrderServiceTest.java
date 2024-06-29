abf45cac041b3778b80e600675f4b880c4fada97
[seongun1] <[seongun44@naver.com]>
2024-06-29 17:48:40 +0900

    6/28 섹션 2까지 완성 6/29 섹션 3까지 완성 - 스프링 config와 DIP 요구 조건을 만족하도록 변경

--- src/test/java/hello/core/Order/OrderServiceTest.java
+++ src/test/java/hello/core/Order/OrderServiceTest.java
@@ -1,18 +1,25 @@
 package hello.core.Order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService ;
+    OrderService orderService ;
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void CreateOrder(){
@@ -20,8 +27,6 @@ public class OrderServiceTest {
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-
-
 
         Order order =  orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService ;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
  // 테스트 코드 작성.
     @Test
     void join(){
